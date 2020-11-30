182700d4cfdc0c67fce55b8b69953d6dde76f1a9
Jeff Manzione <jeffreymanzione@gmail.com>
2020-11-30 20:49:44 +0000

    Adds completer to async module.

--- lib/async.jl
+++ lib/async.jl
@@ -2,6 +2,35 @@ module async
 
 import io
 
-def stub() {
-  io.println('async')
+; Allows for the manual completion of a future.
+;
+; Example:
+; ```
+; do_something = (c) async { c.complete('Hello') }
+; completer = async.Completer()
+; do_something(completer)
+; io.println(await completer.as_future())
+; ```
+class Completer {
+  field completed, _value, _future
+
+  new() {
+    completed = False
+    _value = None
+    _future = () async {
+      while ~(() -> completed)() {}
+      return _value
+    } ()
+  }
+
+  ; Completes the future with the value [v].
+  method complete(v) {
+    _value = v
+    completed = True
+  }
+
+  ; Returns a future to the completed value.
+  method as_future() {
+    return _future
+  }
 }
--- test.jl
+++ test.jl
@@ -1,11 +1,11 @@
 module test
 
 import io
-import math
+import async
 
-io.println(cat('pi=', math.PI, ', e=', math.E))
-io.println(math.max(1, 2, 3, 4, 5), math.min(1, 2, 3, 4, 5))
-io.println(math.pow(2, 4))
-io.println(math.abs(-5), math.abs(4))
-io.println(math.ceil(1.1), math.floor(1.1))
-io.println(math.mod(10.2, 3), 11 % 3)
+
+
+do_something = (c) async { c.complete('Hello, world!') }
+completer = async.Completer()
+do_something(completer)
+io.println(await completer.as_future())
