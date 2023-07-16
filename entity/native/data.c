a3db81a9688875087f71dd3ac383f786d8513898
Jeff <jeffreymanzione@gmail.com>
2023-07-16 12:04:35 -0700

    Add types to data.zeros()

--- entity/native/data.c
+++ entity/native/data.c
@@ -9,10 +9,14 @@
 
 #include "entity/native/data_helper.h"
 
+DEFINE_DATA_ARRAY(Int8Array, int8_t, entity_int);
+DEFINE_DATA_ARRAY(Int32Array, int32_t, entity_int);
 DEFINE_DATA_ARRAY(Int64Array, int64_t, entity_int);
 DEFINE_DATA_ARRAY(Float64Array, double, entity_float);
 
 void data_add_native(ModuleManager *mm, Module *data) {
+  INSTALL_DATA_ARRAY(Int8Array);
+  INSTALL_DATA_ARRAY(Int32Array);
   INSTALL_DATA_ARRAY(Int64Array);
   INSTALL_DATA_ARRAY(Float64Array);
 }
